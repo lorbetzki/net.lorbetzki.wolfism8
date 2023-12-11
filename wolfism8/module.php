e395668b9c1f5948d40e590cf7273911b59fca90
Kristian Lorbetzki <kristian@lorbetzki.net>
2023-12-11 16:24:29 +0100

    fix checks for create and set vars

--- wolfism8/module.php
+++ wolfism8/module.php
@@ -299,8 +299,12 @@ require_once __DIR__ . '/../libs/datapoints.php';
 							$this->MaintainVariable($IPS_IDENT, $IPS_NAME, $DTP['DATAPOINT_IPS_TYPE'], $DTP_Type, $DTP['DATAPOINT_ID'], $CREATEVAR);
 							$this->SendDebug(__FUNCTION__, 'Create variable '. $IPS_IDENT .' with type ' . $DTP_Type, 0);
 						}
+
+						if  (@$this->GetIDForIdent($IPS_IDENT))
+						{
 						$this->SetValue($IPS_IDENT, $DTP_VALUE);
 						$this->SendDebug(__FUNCTION__, 'Set variable with ident ' . $IPS_IDENT . " to value " .$DTP_VALUE , 0);
+						}
 					}
 					// bei Schreibbaren Idents diese Sichtbar machen.
 					if ( ($IPS_IDENT == "DTP_57") || ($IPS_IDENT == "DTP_65") || ($IPS_IDENT == "DTP_194") )
